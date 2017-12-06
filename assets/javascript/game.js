60f49203ae2268e31dc4ae9c9dd2ed80c718920c
phild6596 <phild6596@gmail.com>
2017-12-06 15:44:24 -0800

    <HTML, JS, CSS> > Changed the HTML so I can see print, resized the Star_Wars_words font-size, started over with my JS, the code wasnt as clean as I would like.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,36 +7,58 @@ var words = ["star wars",
     "darth vader",
     "lando calrissian",
     "anakin skywalker",
-    "padme amidala",];
+    "padme amidala"];
 //random words
-var randomWords = Math.floor(Math.random() * words.length);
-var chosenWord = words[randomWords];
+
+//var chosenWord = words[randomWords];
 var rightLetter = [];
 var wrongLetter = [];
 var underScore = [];
-console.log(chosenWord);
+var randomWords = words[Math.floor(Math.random() * words.length)];
+//answer array
+var chosenWords = [];
+for (i = 0; i < randomWords.length; i++) {
+    chosenWords[i] = "_";
+    console.log(chosenWords);
+}
+//Variable that keeps wrong guesses
+var remainingLetters = randomWords.length; 
+
+//Game Loop
+while (remainingLetters < 0) {
+    var PlayerProgress = document.getElementById("Star_Wars_words");
+    PlayerProgress.innerHTML = chosenWords.join(" ");
+}
 
 //create underscores
-function generateUnderscore()  {
-    for (i = 0; i < chosenWord.length; i++) {
-        underScore.push("_");
-     }
-    return underScore;
-}; console.log(generateUnderscore());
+//function generateUnderscore()  {
+  //  for (i = 0; i < chosenWord.length; i++) {
+    //    underScore.push("_");
+     //}
+    //return underScore;
+//}; console.log(generateUnderscore());
 
 //Users guess
-document.addEventListener("keypress", letterPressed); 
+//document.addEventListener("keypress", letterPressed); 
 
-function letterPressed(event) {
-    var letter = String.fromCharCode(event.keyCode);
-    console.log(chosenWord.indexOf(letter));
+//function letterPressed(event) {
+  //  var letter = String.fromCharCode(event.keyCode);
+    //console.log(chosenWord.indexOf(letter));
 
     //if players guess is right
-    if (chosenWord.indexOf(letter) > -1){
+    //if (chosenWord.indexOf(letter) > -1){
     //push into rightWord array
-    rightLetter.push(letter);
-    }
-};
+    //rightLetter.push(letter);
+    //replace underscore with letter
+   // underScore[chosenWord.indexOf(letter)] = letter;
+   // console.log(underScore);
+    //if (underScore.join() == chosenWord){
+   //     alert("The Force is strong with you!");
+   // };
+   /// };
+//};
+document.getElementById("Star_Wars_words").innerHTML = chosenWords;
+document.innerHTML = chosenWords.join(" ");
 
 var playerGuesses = [];
 //check correct guess
